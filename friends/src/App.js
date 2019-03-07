54925faaa4506398d9048f78a0515c246c993924
Kevin Smith <keveightysev@gmail.com>
2019-03-07 15:09:27 -0600

    Set up basic landing page and login form

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to='/public'>Home Page</Link>
+            </li>
+            <li>
+              <Link to='/protected'>Friend List</Link>
+            </li>
+          </ul>
+          <h1>Your Friends List</h1>
+          <Route path="/login" component={Login}/>
+          <PrivateRoute path="/protected" component={Protected} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ friends/src/components/Login.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
+
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    render() {
+        return (
+            <form>
+                <input 
+                    type="text"
+                    name="username"
+                    value={this.state.credentials.username}
+                    onchange={this.handleChange} 
+                />
+                <input 
+                    type="password"
+                    name="password"
+                    value={this.state.credentials.password}
+                    onchange={this.handleChange} 
+                />
+                <button>Log In</button>
+            </form>
+        );
+    }
+}
+
